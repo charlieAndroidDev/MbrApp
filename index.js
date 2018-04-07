eafdf308290b1891bceab26bc1ef6d19b896d0e0
charlieNiekirk <c.niekirk1@ncl.ac.uk>
2018-04-07 17:30:28 -0300

    Created new app

--- index.js
+++ index.js
@@ -1,53 +1,18 @@
 var http = require('http');
-var qs = require('querystring');
-var crypto = require("crypto");
+var fs = require("fs");
 
-var server = http.createServer(function(request, response) {
+fs.readFile('index.html', function (err, html) {
 
-    var start = Date.now();
-
-    if (request.method == 'POST') {
-        var body = '';
-
-
-        request.on('data', function (data) {
-            body += data;
-            if (body.length > 1e6)
-                request.connection.destroy();
-        });
-
-        request.on('end', function () {
-            var post = qs.parse(body);
-
-            var uniqueId = post['unique_id'];
-            var payloadSize = post['payload_size'];
-
-            // Hex string assigns 2 chars to each byte
-            var randomData = crypto.randomBytes(payloadSize * 500).toString('hex');
-            console.log("Data: %s", randomData);
-
-            var obj = new Object();
-            obj.requestId = uniqueId;
-            obj.timestamp = start;
-            obj.payload = randomData;
-            obj.delay = Date.now() - start;
-            var jsonString= JSON.stringify(obj);
-            
-            response.writeHead(200, {"Content-Type": "application/json"});
-            response.end(jsonString);
-        });
-    } else {
+    var server = http.createServer(function(request, response) {
         
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.end("Hello World!");
-
-    }
-
-    
+        response.writeHead(200, {"Content-Type": "text/html"});
+        response.end(html);
+        
+    });
+        
+    var port = process.env.PORT || 1337;
+    server.listen(port);
+        
+    console.log("Server running at http://localhost:%d", port);
 
 });
-
-var port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
