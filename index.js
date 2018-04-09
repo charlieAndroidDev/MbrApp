e540a64b70996117cf927b4fedd3771b834ccf6c
charlieNiekirk <c.niekirk1@ncl.ac.uk>
2018-04-09 16:50:00 -0300

    test

--- index.js
+++ index.js
@@ -14,23 +14,18 @@ var server = http.createServer(function(request, response) {
     //response.writeHead(200, {"Content-Type": "text/html"});
     //response.end(html);
 
+    var body = '';
+    
+    request.on('data', function (data) {
+        body += data;
+    });
+
+    console.log("BODY: " + body);
+
     var urlParts = url.parse(request.url);
     var route = routes[urlParts.pathname];
 
     if(route && request.method == "POST") {
-        var body = '';
-        
-        request.on('data', function (data) {
-            body += data;
-    
-            // Too much POST data, kill the connection!
-            // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
-            if (body.length > 1e6)
-                request.connection.destroy();
-        });
-
-        console.log("BODY: " + body);
-
         route(request, response, body);
     }
   
