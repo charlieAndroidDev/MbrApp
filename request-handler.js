f25fb104bb77774e707832b7a0c187eb2075ef00
charlieNiekirk <c.niekirk1@ncl.ac.uk>
2018-04-09 16:33:18 -0300

    test

--- request-handler.js
+++ request-handler.js
@@ -1,6 +1,7 @@
 var utilities = require('./utilities.js');
 var fs = require("fs");
 var http = require('http');
+var qs = require('querystring');
 
 var actions = {
   'GET': function(request, response) {
@@ -10,9 +11,15 @@ var actions = {
   },
   'POST': function(request, response) {
 
-    var body = "";
-    request.on('data', function(data) {
-        body = data.toString();
+    var body = '';
+    
+    request.on('data', function (data) {
+        body += data;
+
+        // Too much POST data, kill the connection!
+        // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+        if (body.length > 1e6)
+            request.connection.destroy();
     });
 
     var options = {
