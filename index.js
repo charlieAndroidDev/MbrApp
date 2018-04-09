06737a411b8bfd746c91947aef332d54600460d6
charlieNiekirk <c.niekirk1@ncl.ac.uk>
2018-04-09 15:23:36 -0300

    test

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var http = require('http');
 var fs = require("fs");
 var url = require('url');
-var handleRequest = require('./request-handler.js');
-var utilities = require('./utilities.js');
+var handleRequest = require('request-handler.js');
+var utilities = require('utilities.js');
 
 var routes = {
     '/apply': handleRequest
