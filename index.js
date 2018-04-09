ba064e81f7af3936aaf0c85b5ebb4d24a60da1ba
charlieNiekirk <c.niekirk1@ncl.ac.uk>
2018-04-09 14:32:35 -0300

    added express

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
 var http = require('http');
-var fs = require("fs");
+var express = require("express");
+const app = new express();
 
-fs.readFile('index.html', function (err, html) {
-
-    var server = http.createServer(function(request, response) {
-        
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.end(html);
-        
-    });
-        
-    var port = process.env.PORT || 1337;
-    server.listen(port);
-        
-    console.log("Server running at http://localhost:%d", port);
-
+app.get('/', function(request, response){
+    response.sendfile('index.html');
 });
