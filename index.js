0a6824836fec5ca8949c540c8f449cee246cd03e
charlieNiekirk <c.niekirk1@ncl.ac.uk>
2018-04-09 14:46:26 -0300

    overrite

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var http = require('http');
 var express = require("express");
-const app = new express();
+var app = new express();
 
 app.get('/', function(request, response){
     response.sendfile('index.html');
