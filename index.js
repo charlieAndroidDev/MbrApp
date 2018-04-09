477e9edabe031da98f2136703591385bceb0cb1d
charlieNiekirk <c.niekirk1@ncl.ac.uk>
2018-04-09 15:22:41 -0300

    test

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 var http = require('http');
 var fs = require("fs");
+var url = require('url');
+var handleRequest = require('./request-handler.js');
+var utilities = require('./utilities.js');
 
-fs.readFile('index.html', function (err, html) {
+var routes = {
+    '/apply': handleRequest
+};
 
-    var server = http.createServer(function(request, response) {
-        
-        response.writeHead(200, {"Content-Type": "text/html"});
-        response.end(html);
-        
-    });
-        
-    var port = process.env.PORT || 1337;
-    server.listen(port);
-        
-    console.log("Server running at http://localhost:%d", port);
+var server = http.createServer(function(request, response) {
+    
+    //response.writeHead(200, {"Content-Type": "text/html"});
+    //response.end(html);
 
+    var urlParts = url.parse(request.url);
+    var route = routes[parts.pathname];
+  
+    if (route) route(request, response);
+    else utilities.sendResponse(response, 'Not Found', 404);
+    
 });
+    
+var port = process.env.PORT || 1337;
+server.listen(port);
+    
+console.log("Server running at http://localhost:%d", port);
